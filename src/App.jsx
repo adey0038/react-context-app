36e4165c0a6068d4fb92d906d937fb7021a5f75c
Oluwafunke Adeyemo <adey0038@algonquinlive.com>
2026-03-31 19:59:31 -0400

    Complete app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,20 @@ import Header from "./components/header.jsx";
 import SearchBar from "./components/searchBar.jsx";
 import SearchResults from "./components/searchResults.jsx";
 import Locations from "./components/locations.jsx";
-import LocationCard from "./components/locationCard.jsx";
 import Forecast from "./components/forecast.jsx";
-import { useState } from "react";
-import "./App.css";
+import "./index.css";
 
 function App() {
-  const [title, setTitle] = useState("Weather App");
-  const [subtitle, setSubtitle] = useState("Oluwafunke Adeyemo");
-
   return (
     <>
-      <div className="App">
-        <Header title={title} subtitle={subtitle} />
+      <Header />
 
-        <SearchBar />
-        <SearchResults />
+      <SearchBar />
+      <SearchResults />
 
-        <Locations>
-          <LocationCard />
-        </Locations>
+      <Locations />
 
-        <Forecast location={location} />
-      </div>
+      <Forecast />
     </>
   );
 }
